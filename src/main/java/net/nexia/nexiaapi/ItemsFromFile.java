630f54272b819e48c695ddae47d76da273746add
Hector Angelopoulos <62361708+HeadMonitor@users.noreply.github.com>
2023-03-16 00:15:56 +0200

    Minor changes to ItemsFromFile, added the getItemFromSection function.

--- src/main/java/net/nexia/nexiaapi/ItemsFromFile.java
+++ src/main/java/net/nexia/nexiaapi/ItemsFromFile.java
@@ -20,12 +20,12 @@ public class ItemsFromFile
 {
 
     /**
-     * Allows the creation of items from files. Check the wiki for file syntax.
+     * Allows the creation of Items from a File. Check the wiki for file syntax.
      * @param fileName The name of the file. Do not include file extension.
      * @param filePath The path of the file.
-     * @return Returns a list of items from the specified file.
+     * @return Returns a list of Items from the specified File.
      */
-    public static List<ItemStack> GetItems(String fileName, File filePath)
+    public static List<ItemStack> getItemsFromFile(String fileName, File filePath)
     {
         //Items to Return
         List<ItemStack> items = new ArrayList<>();
@@ -46,38 +46,53 @@ public class ItemsFromFile
 
                 if (section == null) return null;
 
-                ItemStack item = new ItemStack(Material.valueOf(section.getString("Type")));
-
-                //Item
-                ItemMeta itemMeta = item.getItemMeta();
-                if (itemMeta == null) return null;
-
-                //Lore
-                List<String> lore = new ArrayList<>();
-                for (String d : section.getStringList("Description"))
-                    lore.add(Processes.color(d));
-
-                //Setting Meta
-                itemMeta.setDisplayName(Processes.color(Objects.requireNonNull(section.getString("Name")))); //Name
-                itemMeta.setLore(lore); //Lore
-
-                for (Map e : section.getMapList("Enchantments")) //Enchantments
-                {
-                    String enchantmentName =  e.values().toArray()[0].toString();
-                    NamespacedKey key = NamespacedKey.fromString("minecraft:" + enchantmentName);
-                    itemMeta.addEnchant(Objects.requireNonNull(Enchantment.getByKey(key)), (Integer) e.get(e.keySet().toArray()[1]), true);
-                }
-
-                if (section.getBoolean("HideEnchants")) itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS); //If enabled hide enchants
-
-                item.setItemMeta(itemMeta);
-
                 //Add item to list
-                items.add(item);
+                items.add(item(section));
             }
         }
 
         return items;
+    }
+
+    /**
+     * Allows the creation of an Item from a Configuration Section.
+     * @param section The Configuration Section to get the Item from.
+     * @return Returns the Item from the specified Section.
+     */
+    public static ItemStack getItemFromSection(ConfigurationSection section)
+    {
+        return item(section);
+    }
+
+    private static ItemStack item(ConfigurationSection section)
+    {
+        ItemStack item = new ItemStack(Material.valueOf(section.getString("Type")));
+
+        //Item
+        ItemMeta itemMeta = item.getItemMeta();
+        if (itemMeta == null) return null;
+
+        //Lore
+        List<String> lore = new ArrayList<>();
+        for (String d : section.getStringList("Description"))
+            lore.add(Processes.color(d));
+
+        //Setting Meta
+        itemMeta.setDisplayName(Processes.color(Objects.requireNonNull(section.getString("Name")))); //Name
+        itemMeta.setLore(lore); //Lore
+
+        for (Map e : section.getMapList("Enchantments")) //Enchantments
+        {
+            String enchantmentName =  e.values().toArray()[0].toString();
+            NamespacedKey key = NamespacedKey.fromString("minecraft:" + enchantmentName);
+            itemMeta.addEnchant(Objects.requireNonNull(Enchantment.getByKey(key)), (Integer) e.get(e.keySet().toArray()[1]), true);
+        }
+
+        if (section.getBoolean("HideEnchants")) itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS); //If enabled hide enchants
+
+        item.setItemMeta(itemMeta);
+
+        return item;
     }
 
 }
