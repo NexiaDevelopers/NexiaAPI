634e95bdcc07e1acce083f8561f7a5f681d039cf
Gepsu <joni.nyqvist@netikka.fi>
2025-09-01 23:14:03 +0300

    Added alternate color code method

---
+++ src/main/java/net/nexia/nexiaapi/util/Utils.java
@@ -0,0 +1,9 @@
+package net.nexia.nexiaapi.util;
+
+import org.bukkit.ChatColor;
+
+public class Utils {
+
+    public static String color (String string) { return ChatColor.translateAlternateColorCodes('&', string); }
+
+}
